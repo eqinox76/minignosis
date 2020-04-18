9aeadb3348c122ce56a41c5b9745f67220b8ee5b
eqinox76 <5861557+eqinox76@users.noreply.github.com>
2020-04-18 14:08:35 +0200

    feat: logout button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import * as React from "react"
-import { Button, List, ListItem, ListItemText } from "@material-ui/core";
+import { AppBar, Button, IconButton, List, ListItem, ListItemText, Toolbar } from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
 import firebase from "firebase";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
+
+function logout() {
+  firebase.auth().signOut().then(function () {
+    window.location.reload();
+  }, function (error) {
+    console.log(error);
+  });
+}
 
 export function App() {
   let provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().getRedirectResult().then(function (result) {
     if (result.user) {
-      console.log(`user logged in ${result.user}`);
+      console.log(`user logged in ${result.user.email}`);
     } else if (firebase.auth().currentUser) {
       console.log(`already logged in user: ${firebase.auth().currentUser.email}`);
     } else {
@@ -14,8 +39,22 @@ export function App() {
     }
   });
 
+  const classes = useStyles();
   return (
     <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon/>
+          </IconButton>
+          <div className={classes.title}>
+            MiniGnosis
+          </div>
+          <IconButton color="inherit" aria-label="sign out" onClick={logout}>
+            <PersonOutlineIcon/>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
       <Button variant="contained" color="primary">
         Hello World!!
       </Button>
